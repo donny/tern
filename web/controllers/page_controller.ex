8c50fd4ddaa8a5ad32d1b81cd4310bd871071cad
Donny Kurniawan <donny.kurniawan@gmail.com>
2017-05-18 14:12:26 +1000

    Add excuses

--- web/controllers/page_controller.ex
+++ web/controllers/page_controller.ex
@@ -2,6 +2,15 @@ defmodule Tern.PageController do
   use Tern.Web, :controller
 
   def index(conn, _params) do
-    render conn, "index.html"
+    excuses = [
+      "I have a fever",
+      "My back pain is aching",
+      "I have a viral or bacterial illness that’s contagious",
+      "I am on strong medication that may affect my reaction time",
+      "My illness prevents me from concentrating",
+      "I am coughing, a lot"
+    ]
+
+    render conn, "index.html", excuse: Enum.random(excuses)
   end
 end
